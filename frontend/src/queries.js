3d5b4ae62b958c57698319412e62e99490f1e0ca
iemeli <eemeli@paalimaki.fi>
2020-03-23 19:34:35 +0200

    Added fragments to queries

--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -1,51 +1,71 @@
 import { gql } from '@apollo/client'
 
+const INGR_DETAILS = gql`
+  fragment IngrDetails on Ingredient {
+    id
+    name
+    price
+    kcal
+  }
+`
 
+const FOOD_DETAILS = gql`
+  fragment FoodDetails on Food {
+    id
+    name
+    price
+    kcal
+    recipe
+    ingredientsCount
+    ingredients {
+      id
+      usedAtOnce
+      item {
+        ...IngrDetails
+      }
+    }
+  }
+  ${INGR_DETAILS}
+`
+
+const FOODPACK_DETAILS = gql`
+  fragment FoodPackDetails on FoodPack {
+    id
+    name
+    price
+    kcal
+    foodsCount
+    foods {
+      ...FoodDetails
+    }
+  }
+  ${FOOD_DETAILS}
+`
 
 export const ALL_INGREDIENTS = gql`
   query allIngredients ($name: String){
     allIngredients (name: $name) {
-      id
-      name
-      price
-      kcal
+      ...IngrDetails
     }
   }
+  ${INGR_DETAILS}
 `
 
 export const ALL_FOODS = gql`
   query allFoods ($name: String) {
     allFoods(name: $name) {
-      id
-      name
-      price
-      kcal
-      recipe
-      ingredientsCount
-      ingredients {
-        id
-        usedAtOnce
-        item {
-          id
-          name
-        }
+        ...FoodDetails
       }
     }
   }
+  ${FOOD_DETAILS}
 `
 
 export const ALL_FOODPACKS = gql`
   query {
     allFoodPacks {
-      id
-      name
-      price
-      kcal
-      foodsCount
-      foods {
-        id
-        name  
-      }
+      ...FoodPackDetails
     }
   }
+  ${FOODPACK_DETAILS}
 `
