7b122a6852c3bd33502d2b7127fb5df56d2e2921
iemeli <eemeli@paalimaki.fi>
2020-05-30 19:36:01 +0300

    Updated folder structure

---
+++ build/precache-manifest.36a9cf14c22d65a534d179d5757e4856.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb56f9616437bee5cb384eaa6e2c62c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899dbb51bb377ba854a9",
+    "url": "/static/css/main.bd2bbdda.chunk.css"
+  },
+  {
+    "revision": "df2f71bd3d8e34947c5a",
+    "url": "/static/js/2.d5c2f118.chunk.js"
+  },
+  {
+    "revision": "ca07fff9a78848f7c2ceb7aa593b294d",
+    "url": "/static/js/2.d5c2f118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899dbb51bb377ba854a9",
+    "url": "/static/js/main.a165fdab.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "156ed18105d0208b5890595ff54c9270",
+    "url": "/static/media/AlegreyaSans-BoldItalic.156ed181.ttf"
+  },
+  {
+    "revision": "4a888da2b1d11ef95c86bc00f5904b3d",
+    "url": "/static/media/AlegreyaSans-Regular.4a888da2.ttf"
+  },
+  {
+    "revision": "3487ab4b3005ae50b93dca57eaada044",
+    "url": "/static/media/list.3487ab4b.svg"
+  }
+]);
