153328c04bc9d77842cd7ae483a7b3a0ea14a79f
iemeli <eemeli@paalimaki.fi>
2020-05-30 20:23:50 +0300

    Changed CSP in index.html

---
+++ build/precache-manifest.2dec20852ae98f7a7fb7e6615d528769.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22737ee6ada222cf5d57a5d389ef495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d158e5cc2329cb38bd",
+    "url": "/static/css/main.bd2bbdda.chunk.css"
+  },
+  {
+    "revision": "4fd40f3aebcfd7f9d4fa",
+    "url": "/static/js/2.56300bfb.chunk.js"
+  },
+  {
+    "revision": "ca07fff9a78848f7c2ceb7aa593b294d",
+    "url": "/static/js/2.56300bfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d158e5cc2329cb38bd",
+    "url": "/static/js/main.43bf0f68.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "156ed18105d0208b5890595ff54c9270",
+    "url": "/static/media/AlegreyaSans-BoldItalic.156ed181.ttf"
+  },
+  {
+    "revision": "4a888da2b1d11ef95c86bc00f5904b3d",
+    "url": "/static/media/AlegreyaSans-Regular.4a888da2.ttf"
+  },
+  {
+    "revision": "3487ab4b3005ae50b93dca57eaada044",
+    "url": "/static/media/list.3487ab4b.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f31170e6cbb3928f990ddfc32666cfbd.js"
+  "/precache-manifest.2dec20852ae98f7a7fb7e6615d528769.js"
 );
 
 self.addEventListener('message', (event) => {
