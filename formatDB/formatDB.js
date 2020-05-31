f3f8fcfa3fb1dbc8bc7c9744df2f8f9b88b0f5be
iemeli <eemeli@paalimaki.fi>
2020-05-31 12:53:41 +0300

    Fixed bug in formatdb.js

--- formatDB/formatDB.js
+++ formatDB/formatDB.js
@@ -22,9 +22,9 @@ mongoose
     console.log('error connection to MongoDB', e.message)
   })
 
-const getIngredientForDB = args => {
+const getIngredientForDB = i => {
   const ingredientForDB = {}
-  Object.keys(args).forEach(key => {
+  Object.keys(i).forEach(key => {
     if (
       [
         'kcal',
@@ -37,16 +37,16 @@ const getIngredientForDB = args => {
       ].indexOf(key) > -1
     ) {
       ingredientForDB[key] = {
-        total: Number((args[key] * (args.weight / 100)).toFixed(1)),
-        in100g: args[key],
-        ...(args.pieces && {
+        total: Number((i[key] * (i.weight / 100)).toFixed(1)),
+        in100g: i[key],
+        ...(i.pieces && {
           inOnePiece: Number(
-            ((args[key] * (args.weight / 100)) / args.pieces).toFixed(1)
+            ((i[key] * (i.weight / 100)) / i.pieces).toFixed(1)
           ),
         }),
       }
     } else {
-      ingredientForDB[key] = args[key]
+      ingredientForDB[key] = i[key]
     }
   })
 
@@ -55,12 +55,16 @@ const getIngredientForDB = args => {
     max: Math.max(...ingredientForDB.prices),
   }
 
-  ingredientForDB.foundInStores = []
-
   return ingredientForDB
 }
 
 const deleteAll = async () => {
+  try {
+    await GroceryStore.deleteMany({})
+    console.log('GroceryStores deleted')
+  } catch (e) {
+    console.log('Error deleting GroceryStores')
+  }
   try {
     await Ingredient.deleteMany({})
     console.log('Ingredients deleted')
@@ -79,135 +83,131 @@ const deleteAll = async () => {
   } catch (e) {
     console.log('Error deleting FoodPacks')
   }
-  try {
-    await GroceryStore.deleteMany({})
-    console.log('GroceryStores deleted')
-  } catch (e) {
-    console.log('Error deleting GroceryStores')
-  }
 }
 
 const format = async () => {
+  await deleteAll()
+
   try {
-    await deleteAll()
+    await groceryStores.forEach(async gs => {
+      await new GroceryStore({
+        name: gs.name,
+        ingredients: [],
+      }).save()
+    })
 
-    try {
-      const ingredientsForDB = ingredients.map(i => getIngredientForDB(i))
-      console.log('Ingredientsfordb', ingredientsForDB)
-      await Ingredient.insertMany(ingredientsForDB)
-      console.log('Ingredients inserted')
-    } catch (e) {
-      console.log('Error inserting ingredients', e.message)
-    }
-    try {
-      const ingredientsFromDB = await Ingredient.find({})
-      const soijaNuudeli = foods[0]
-      soijaNuudeli.ingredients.forEach(i => {
-        const ingredientId = ingredientsFromDB.find(
-          ingr => ingr.name === i.item
-        ).id
-        i.item = ingredientId
-      })
+    await GroceryStore.find({})
 
-      const aamiaisLeipa = foods[1]
-      aamiaisLeipa.ingredients.forEach(i => {
-        const ingredientId = ingredientsFromDB.find(
-          ingr => ingr.name === i.item
-        ).id
-        i.item = ingredientId
-      })
+    console.log('GroceryStores inserted')
+  } catch (e) {
+    console.log('Error inserting Grocery Stores: ', e.message)
+  }
 
-      const food1 = new Food(soijaNuudeli)
-      const food2 = new Food(aamiaisLeipa)
-      await food1.save()
-      await food2.save()
+  try {
+    const ingredientsForDB = ingredients.map(i => getIngredientForDB(i))
 
-      const soijaNuudeliFromDB = await Food.findOne({ name: soijaNuudeli.name })
+    const groceryStoresFromDB = await GroceryStore.find({})
 
-      soijaNuudeliFromDB.ingredients
-        .map(i => i.item.toString())
-        .forEach(async ingrID => {
-          try {
-            const ingredient = await Ingredient.findOne({ _id: ingrID })
-            ingredient.usedInFoods.push(soijaNuudeliFromDB.id)
-            await ingredient.save()
-          } catch (e) {
-            console.log('Error putting foodrefs in ingredients', e.message)
-          }
-        })
-
-      const aamiaisLeipaFromDB = await Food.findOne({ name: aamiaisLeipa.name })
-
-      aamiaisLeipaFromDB.ingredients
-        .map(i => i.item.toString())
-        .forEach(async ingrID => {
-          try {
-            const ingredient = await Ingredient.findOne({ _id: ingrID })
-            ingredient.usedInFoods.push(aamiaisLeipaFromDB.id)
-            await ingredient.save()
-          } catch (e) {
-            console.log('Error putting foodrefs to ingredients', e.message)
-          }
-        })
-
-      console.log('Foods inserted')
-    } catch (e) {
-      console.log('Error inserting foods', e.message)
-    }
-    try {
-      const foodsFromDB = await Food.find({})
-
-      const kurjatSetit = foodPacks[0]
-      kurjatSetit.foods = kurjatSetit.foods.map(
-        f => foodsFromDB.find(ffdb => ffdb.name === f).id
+    ingredientsForDB.forEach(i => {
+      const refStore = groceryStoresFromDB.find(
+        gs => gs.name === i.foundInStores[0]
       )
 
-      await new FoodPack(kurjatSetit).save()
+      i.foundInStores = [refStore.id]
+    })
 
-      const foodPackFromDB = await FoodPack.findOne({})
+    await Ingredient.insertMany(ingredientsForDB)
 
-      const foodPackID = foodPackFromDB.id
+    const ingredientsFromDB = await Ingredient.find({})
 
-      await foodsFromDB.forEach(async f => {
-        f.usedInFoodPacks.push(foodPackID)
-        await f.save()
-      })
-      console.log('Foodpacks inserted')
-    } catch (e) {
-      console.log('Error inserting foodPacks', e.message)
-    }
-    try {
-      const ingredientsFromDB = await Ingredient.find({})
+    await groceryStoresFromDB.forEach(async gs => {
+      const ids = ingredientsFromDB
+        .filter(ifdb => ifdb.foundInStores.includes(gs.id))
+        .map(i => i.id)
+      gs.ingredients.push(...ids)
+      await gs.save()
+    })
 
-      await new GroceryStore({
-        name: groceryStores[0].name,
-        ingredients: groceryStores[0].ingredients.map(
-          i => ingredientsFromDB.find(ifdb => ifdb.name === i).id
-        ),
-      }).save()
-
-      const gsFromDB = await GroceryStore.findOne({})
-
-      gsFromDB.ingredients
-        .map(i => i.toString())
-        .forEach(async ingrdID => {
-          try {
-            console.log('täs ingrdId', ingr)
-            const ingr = await Ingredient.findOne({ _id: ingrdID })
-            ingr.foundInStores.push(gsFromDB.id)
-            await ingr.save()
-          } catch (e) {
-            console.log('Error putting GS-refs to ingredients')
-          }
-        })
-      console.log('GroceryStores inserted')
-    } catch (e) {
-      console.log('Error inserting Grocery Stores')
-    }
+    console.log('Ingredients inserted')
   } catch (e) {
-    console.log('Something wrong in format()', e.message)
-  } finally {
-    mongoose.connection.close()
+    console.log('Error inserting ingredients', e.message)
+  }
+
+  try {
+    const ingredientsFromDB = await Ingredient.find({})
+    const soijaNuudeli = foods[0]
+    soijaNuudeli.ingredients.forEach(i => {
+      const ingredientId = ingredientsFromDB.find(ingr => ingr.name === i.item)
+        .id
+      i.item = ingredientId
+    })
+
+    const aamiaisLeipa = foods[1]
+    aamiaisLeipa.ingredients.forEach(i => {
+      const ingredientId = ingredientsFromDB.find(ingr => ingr.name === i.item)
+        .id
+      i.item = ingredientId
+    })
+
+    const food1 = new Food(soijaNuudeli)
+    const food2 = new Food(aamiaisLeipa)
+    await food1.save()
+    await food2.save()
+
+    const soijaNuudeliFromDB = await Food.findOne({ name: soijaNuudeli.name })
+
+    soijaNuudeliFromDB.ingredients
+      .map(i => i.item.toString())
+      .forEach(async ingrID => {
+        try {
+          const ingredient = await Ingredient.findOne({ _id: ingrID })
+          ingredient.usedInFoods.push(soijaNuudeliFromDB.id)
+          await ingredient.save()
+        } catch (e) {
+          console.log('Error putting foodrefs in ingredients', e.message)
+        }
+      })
+
+    const aamiaisLeipaFromDB = await Food.findOne({ name: aamiaisLeipa.name })
+
+    aamiaisLeipaFromDB.ingredients
+      .map(i => i.item.toString())
+      .forEach(async ingrID => {
+        try {
+          const ingredient = await Ingredient.findOne({ _id: ingrID })
+          ingredient.usedInFoods.push(aamiaisLeipaFromDB.id)
+          await ingredient.save()
+        } catch (e) {
+          console.log('Error putting foodrefs to ingredients', e.message)
+        }
+      })
+
+    console.log('Foods inserted')
+  } catch (e) {
+    console.log('Error inserting foods', e.message)
+  }
+
+  try {
+    const foodsFromDB = await Food.find({})
+
+    const kurjatSetit = foodPacks[0]
+    kurjatSetit.foods = kurjatSetit.foods.map(
+      f => foodsFromDB.find(ffdb => ffdb.name === f).id
+    )
+
+    await new FoodPack(kurjatSetit).save()
+
+    const foodPackFromDB = await FoodPack.findOne({})
+
+    const foodPackID = foodPackFromDB.id
+
+    await foodsFromDB.forEach(async f => {
+      f.usedInFoodPacks.push(foodPackID)
+      await f.save()
+    })
+    console.log('Foodpacks inserted')
+  } catch (e) {
+    console.log('Error inserting foodPacks', e.message)
   }
 }
 
