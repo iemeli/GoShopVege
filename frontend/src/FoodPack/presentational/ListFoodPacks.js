1093dd7896787864b6420ae03531cd96fa51e59d
iemeli <eemeli@paalimaki.fi>
2020-05-31 22:00:29 +0300

    Changed FoodPack listing back to using bootstrap

--- frontend/src/FoodPack/presentational/ListFoodPacks.js
+++ frontend/src/FoodPack/presentational/ListFoodPacks.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button'
-import ShopListButton from '../../ShopList/ShopListButton'
-import '../../css/contentCard.css'
+import ShopListCard from './ShopListCard'
 
 const ListFoodPacks = props => (
   <div>
     {props.foodPacks.map(fp => (
-      <div
-        className="foodPackCard"
-        style={{ width: '18rem', margin: '1rem' }}
-        key={fp.id}
-      >
-        {fp.name}
-      </div>
+      <ShopListCard foodPack={fp} id={fp.id} buttons />
     ))}
   </div>
 )
